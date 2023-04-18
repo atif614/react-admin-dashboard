8fe016f8181befaabdadc9e67e9663780fa0a237
atif614 <razaatif20786@gmail.com>
2023-04-18 10:09:54 +0530

    Initial commit

--- src/scenes/Global/Sidebar.jsx
+++ src/scenes/Global/Sidebar.jsx
@@ -1,3 +1,6 @@
+import { useState } from "react";
+import { ProSidebar,Menu,MenuItem } from "react-pro-sidebar";
+import 'react-pro-sidebar/dist/css/styles.css';
 
 const Sidebar = ()=>{
     return(
